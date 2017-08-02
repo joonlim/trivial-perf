1f8593d989a4190108012c3b43db2ce2c36ec3bd
Oren Teich <oren@teich.net>
2017-08-01 21:07:28 -0700

    playing with artillery

---
+++ hello/lambda/handler.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports.helloWorld = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Force a deploy',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
